9e274ad0d0880bb858e988a3d0ca8069a0b2a4b0
Ramiro Antelo <ramiro.antelo4@gmail.com>
2026-07-01 17:52:55 -0300

    remove progressiveimage

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -3,28 +3,30 @@ import { Pressable, Text, View } from "react-native";
 import { PokemonListItem } from "../../types/pokemon";
 import { formatPokemonId } from "../../utils/format";
 import { styles } from "./styles";
-import ProgressiveImage from "./components";
+import { Image } from "expo-image";
 
 interface Props {
-  pokemon: PokemonListItem;
-  onPress?: () => void;
+	pokemon: PokemonListItem;
+	onPress?: () => void;
 }
 
 function PokemonCard({ pokemon, onPress }: Props) {
-  return (
-    <Pressable style={styles.card} onPress={onPress}>
-      <ProgressiveImage
-        lowRes={pokemon.sprite}
-        highRes={pokemon.image}
-        style={styles.image}
-      />
-
-      <View style={styles.info}>
-        <Text style={styles.id}>{formatPokemonId(pokemon.id)}</Text>
-        <Text style={styles.name}>{pokemon.name}</Text>
-      </View>
-    </Pressable>
-  );
+	return (
+		<Pressable style={styles.card} onPress={onPress}>
+			<Image
+				source={{ uri: pokemon.image }}
+				style={styles.image}
+				contentFit="contain"
+				cachePolicy="memory-disk"
+				priority="normal"
+				transition={150}
+			/>
+			<View style={styles.info}>
+				<Text style={styles.id}>{formatPokemonId(pokemon.id)}</Text>
+				<Text style={styles.name}>{pokemon.name}</Text>
+			</View>
+		</Pressable>
+	);
 }
 
 export default React.memo(PokemonCard);
